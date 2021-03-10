6e4b5bcbecc5d74d28f42a6b12dd83c27b1e3953
mtheusbrito <matheusbritodasilva@gmail.com>
2021-03-10 00:34:17 -0300

    configurando babel

---
+++ dist/bundle.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// import { Container } from './styles';
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello world");
+}
+
+var _default = App;
+exports["default"] = _default;
