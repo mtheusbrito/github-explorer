7e67a0320a7df8d734fec204b443f537da853ce8
mtheusbrito <matheusbritodasilva@gmail.com>
2021-03-24 22:24:59 -0300

    utilizando sass

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,23 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
-  devtool: isDevelopment ? 'eval-source-map': 'source-map', 
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
- 
+
   resolve: {
     extensions: ['.js', '.jsx'],
-  }, 
+  },
   devServer: {
-    contentBase: path.resolve(__dirname, 'public', 'index.html')
+    contentBase: path.resolve(__dirname, 'public', 'index.html'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public' , 'index.html')
-    })
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
   ],
   module: {
     rules: [
@@ -28,6 +28,11 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
