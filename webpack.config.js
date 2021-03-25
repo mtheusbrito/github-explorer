405c07047fa26271f29e625240558701dc089c90
mtheusbrito <matheusbritodasilva@gmail.com.br>
2021-03-25 11:09:59 -0300

    estado do componente

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path')
-<<<<<<< HEAD
-
-const isDevelopment = process.env.NODE_ENV !== 'productiono';
-module.exports = {
-  mode: isDevelopment ? 'development' : 'production',
-  devtool: isDevelopment ? 'eval-source-map' : 'source-map', 
-=======
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map',
->>>>>>> ad0c635f7b9f977b41945544b625de3200b03f4f
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-
   resolve: {
     extensions: ['.js', '.jsx'],
   },
